d87ff86d8742224611e1930979c4909faee1cb2f
trackmateai2 <trackmateai2@gmail.com>
2024-09-07 16:19:27 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -2,28 +2,39 @@
 function feedBack(){
 
              
-    
-    var username= document.getElementById("email").value;
-    
-    var myLoginLay = document.getElementById("LoginLay");
-    var myMainLay = document.getElementById("MainLay");
-    var myInfoLay = document.getElementById("InfoLay");
-
+        var username= document.getElementById("email").value;
+        var myLoginLay = document.getElementById("LoginLay");
+        var myMainLay = document.getElementById("MainLay");
+        var myInfoLay = document.getElementById("InfoLay");
             myMainLay.style.display=("none");
             myLoginLay.style.display=("none");
             myInfoLay.style.display=("block");
+               var imei = document.getElementById("imeiCont");
+               var model = document.getElementById("modelCont");
+                   phoneImageLay= document.getElementById("imageLay");
+                   phoneImage= document.getElementById("phoneImage");
+      
+   
+
+ if (username=="gmasele15@gmail.com"){
+            phoneImageLay.style.display=("block");
+            document.getElementById("unlockBtn").innerHTML = "UnLock Full Intel(P250)";
+            
+            imei.innerHTML=(" 123098 ");
+            model.innerHTML=(" Samsung Galaxy A54 (ADKJ6)");
+            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a13.jpg";
     
-    var mainInfo = document.getElementById("mainInfo");
-    var moreInfo = document.getElementById("moreInfo");
+        
+   }
 
-    
-                phoneImageLay= document.getElementById("imageLay")
-                phoneImage= document.getElementById("phoneImage")
-
-
-
-  
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 if (username=="selokotinabo@gmail.com"){
             window.location.href=("resultsTinabo.html");
    }
@@ -57,31 +68,7 @@ if (username=="74331061@temp.com"){
  if (username=="tempclient52@gmail.com"){
             window.location.href=("resultsAlvaro.html");
    }
-
- if (username=="obuisanyang@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/d/n/ZCr4E2-d.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 864743060482791</font><br/>  Model: Nova Y91(STG-LX2)<br/>Brand: HUAWEI <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana  ");
-        moreInfo.innerHTML=("  Operating System	Android 13<br/>Chipset	Qualcomm Snapdragon 680 SM6225<br/>Number of CPU Cores	4<br/>RAM	8 GB<br/> Width	79.9 mm<br/>Height	171.6 mm<br/>Thickness	8.9 mm<br/>Weight	214.0 g<br/> DisplayType	LCD_IPS<br/>Diagonal	6.95 inches<br/>Resolution	1080x2376 px<br/>Screen-to-body ratio	82.6 %<br/>Pixel density	376 ppi<br/>Colors	16M   ");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
-  if (username=="Nkuramosweu@gmail.com"){
-            phoneImageLay.style.display=("block");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11.jpg";
-    
-        mainInfo.innerHTML=(" STATUS: <font color=#00ee00>Found</font><br/>IMEI: <font color=#00ee00> 356545108161419</font><br/>  Model: iPhone 11(A2111)<br/>Brand: APPLE <br/>DeviceType:  Smartphone<br/>  Location: <font color=#ff0000>  Hidden(Payment Required)</font><br/>  Country Code: BW<br/>  Country Name: Botswana ");
-        moreInfo.innerHTML=(" Operating System	iOS 17<br/>Chipset	Apple A13 Bionic<br/>Number of CPU Cores	6<br/>RAM	4 GB<br/> Width	75.7 mm<br/>Height	150.9 mm<br/>Thickness	8.3 mm<br/>Weight	194.0 g");
-        
-        mainInfo.style.display=("block");
-        moreInfo.style.display=("block");
-   }
- if (username=="gmasele15@gmail.com"){
-             window.location.href=("results729884.html");
-   }
- if (username=="facethebushman@yahoo.com"){
+if (username=="facethebushman@yahoo.com"){
             window.location.href=("resultsGabriel3551.html");
    }
   
@@ -89,8 +76,10 @@ if (username=="74331061@temp.com"){
  if (username=="flockie2002@gmail.com"){
           window.location.href=("resultsFlockie35749.html");
    }
-  
-
+   
+   
+   
+ 
 }
 
 
