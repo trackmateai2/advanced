7f3f5ae8acd1c65ec7fc8b5af3d3a67c6822e786
trackmateai2 <trackmateai2@gmail.com>
2024-09-24 17:21:56 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -59,37 +59,14 @@ function feedBack(){
   if (username=="tlhalefowaloka@gmail.com"){
              window.location.href=("resultsTlhalefo.html");
    }
-  if (username=="ogaatlale@gmail.com"){
-            phoneImageLay.style.display=("block");
-            document.getElementById("unlockBtn").innerHTML = "UnLock Full Intel(P250)";
-            
-            imei.innerHTML=("353102450743569");
-            model.innerHTML=(" TECNO Spark 10 5G");
-            phoneImage.src = "https://imei.net/phone-images/t/gsm-cache/x/h/HbJQI8-d.jpg";
-   }
- if (username=="gmasele15@gmail.com"){
-            window.location.href=("resultsTlhalefo.html");
-   }
- if (username=="shallahmagosi@gmail.com"){
-            window.location.href=("resultsYin.html");
-   }
+
  if (username=="75900844@gmail.com"){
             window.location.href=("resultsChax.html");
    }
  if (username=="72480212@temp.com"){
             window.location.href=("results72480.html");
    }
- if (username=="egarwe@hotmail.com"){
-            phoneImageLay.style.display=("block");
-            document.getElementById("unlockBtn").innerHTML = "UnLock Full Intel(P250)";
-            
-            imei.innerHTML=("358628621682158");
-            model.innerHTML=(" SAMSUNG Galaxy S22 Ultra 5G(SM-S908B)");
-            phoneImage.src = "https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-s22-ultra-5g.jpg";
-   }
- if (username=="ebbie@temp.com"){
-            window.location.href=("resultsEbbie.html");
-   }
+
   if (username=="karabotumi@gmail.com"){
             phoneImageLay.style.display=("block");
             document.getElementById("unlockBtn").innerHTML = "UnLock Full Intel(P250)";
@@ -106,9 +83,6 @@ function feedBack(){
 
 
   
-  if (username=="thuto2018th@gmail.com"){
-            window.location.href=("resultsThuto35415.html");
-   }
 
  
  if (username=="obuisanyang@gmail.com"){
@@ -135,35 +109,14 @@ if (username=="selokotinabo@gmail.com"){
             window.location.href=("results76984.html");
    }
   
- if (username=="kopanotill@yahoo.com"){
-            window.location.href=("resultsKopanotill.html");
-   }
 if (username=="74331061@temp.com"){
             window.location.href=("results74331.html");
-   }
-  if (username=="kizzysekai@temp.com"){
-            window.location.href=("resultsKopanotill.html");
-   }
-   if (username=="shaunmosige0@gmail.com"){
-            window.location.href=("resultsShaun2.html");
-   }
-   if (username=="72988464@tempmail.com"){
-            window.location.href=("results729884.html");
-   }
-   if (username=="kellzjnr@live.com"){
-            window.location.href=("resultsKellz.html");
    }
  if (username=="tempclient52@gmail.com"){
             window.location.href=("resultsAlvaro.html");
    }
-if (username=="facethebushman@yahoo.com"){
-            window.location.href=("resultsGabriel3551.html");
-   }
   
     
- if (username=="flockie2002@gmail.com"){
-          window.location.href=("resultsFlockie35749.html");
-   }
    
    
    
