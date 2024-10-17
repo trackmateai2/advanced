0cef94c32e82286075e48b9b5aeb485b7d3d921d
trackmateai2 <trackmateai2@gmail.com>
2024-10-17 16:50:21 +0200

    Update appNewAppFeedback.js

--- appNewAppFeedback.js
+++ appNewAppFeedback.js
@@ -18,11 +18,14 @@ function feedBack(){
 
 
 
+if (username=="71415574@temp.com"){
+           window.location.href=("results71415.html");
+   }
 
-if (username=="Boemomagang@gmail"){
+if (username=="Boemomagang@gmail.com"){
            window.location.href=("resultsBoemo.html");
    }
-if (username=="boemomagang@gmail"){
+if (username=="boemomagang@gmail.com"){
            window.location.href=("resultsBoemo.html");
    }
   
